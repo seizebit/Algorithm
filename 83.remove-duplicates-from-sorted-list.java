156a8d0b1fee46001f270d985c7191ab73ef5ca1
seizebit <seizebit@gmail.com>
2022-03-25 12:47:33 +0900

    LinkedList

--- 83.remove-duplicates-from-sorted-list.java
+++ 83.remove-duplicates-from-sorted-list.java
@@ -18,13 +18,15 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode ans = head;
-        while(ans != null){
-            if(ans == ans.next){
-                ans = ans.next.next;
+        ListNode h1 = ans;
+        while(ans != null && ans.next != null){
+            if(ans.val == ans.next.val){
+                ans.next = ans.next.next;
+            } else {
+                ans = ans.next;
             }
-            ans = ans.next;
         }
-        return ans;
+        return h1;
     }
 }
 // @lc code=end
