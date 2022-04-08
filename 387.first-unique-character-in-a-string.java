a776dbef247867a1353ed633003fac8472666a2e
seizebit <seizebit@gmail.com>
2022-04-08 09:43:50 +0900

    adasda

---
+++ 387.first-unique-character-in-a-string.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=387 lang=java
+ *
+ * [387] First Unique Character in a String
+ */
+
+// @lc code=start
+class Solution {
+    public int firstUniqChar(String s) {
+        
+    }
+}
+// @lc code=end
+
